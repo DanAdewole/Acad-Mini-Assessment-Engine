9f88cb087aede0c6c8bcec8dfd911352a6ff38a2
Daniel Adewole <adedaniel504@gmail.com>
2026-01-06 17:11:03 +0100

    delete unused exceptions

--- assessment_engine/exceptions.py
+++ assessment_engine/exceptions.py
@@ -1,5 +1,3 @@
-from rest_framework import status
-from rest_framework.exceptions import APIException
 from rest_framework.views import exception_handler as drf_exception_handler
 
 
@@ -37,35 +35,3 @@ def get_error_message(exc):
             return exc.detail[0] if exc.detail else str(exc)
         return str(exc.detail)
     return str(exc)
-
-
-class ExamNotAvailableException(APIException):
-    """Exception raised when exam is not available for submission."""
-
-    status_code = status.HTTP_403_FORBIDDEN
-    default_detail = "This exam is not available for submission at this time."
-    default_code = "exam_not_available"
-
-
-class SubmissionAlreadyExistsException(APIException):
-    """Exception raised when user tries to submit exam twice."""
-
-    status_code = status.HTTP_400_BAD_REQUEST
-    default_detail = "You have already submitted this exam."
-    default_code = "submission_exists"
-
-
-class InvalidAnswerException(APIException):
-    """Exception raised when answer format is invalid."""
-
-    status_code = status.HTTP_400_BAD_REQUEST
-    default_detail = "Invalid answer format."
-    default_code = "invalid_answer"
-
-
-class GradingException(APIException):
-    """Exception raised when grading fails."""
-
-    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-    default_detail = "An error occurred during grading."
-    default_code = "grading_error"
